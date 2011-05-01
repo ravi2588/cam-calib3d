5cfc2f03416f117ffdb3e7e773b092264379b561
christoph.heindl <christoph.heindl@99fb1f6b-97a0-cadf-a99d-5275acf9f181>
2011-05-01 18:13:22 +0000

    added initial calibration support.

---
+++ trunk/Calib3D/Calibration.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Calib3D {
+
+  /// <summary>
+  /// Perform intrinsic/extrinsic camera calibration from point correspondences.
+  /// </summary>
+  public static class Calibration {
+
+    /// <summary>
+    /// Caluculate instrinsic camera parameters from correspondences.
+    /// </summary>
+    /// <remarks>
+    /// Besides the intrinsic camera parameters, extrinsic camera parameters are returned
+    /// for each view in the correspondence set.
+    /// </remarks>
+    /// <param name="c">Model/image correspondences for multiple views</param>
+    /// <param name="image_size">Size of source image</param>
+    /// <returns>Calibration result</returns>
+    public static CalibrationResult CalculateIntrinsics(Correspondences c, System.Drawing.Size image_size) {
+      if (c.CorrespondenceCount < 4)
+        throw new ArgumentException("At least four correspondences are required");
+
+      CalibrationResult cr = new CalibrationResult();
+      cr.Correspondences = c;
+      cr.Intrinsics = new Emgu.CV.IntrinsicCameraParameters();
+
+      Emgu.CV.ExtrinsicCameraParameters[] ext;
+
+      cr.ReprojectionError = (float)Emgu.CV.CameraCalibration.CalibrateCamera(
+        c.ModelPoints, c.ImagePoints, image_size,
+        cr.Intrinsics,
+        Emgu.CV.CvEnum.CALIB_TYPE.DEFAULT,
+        out ext);
+
+      cr.Extrinsics = ext;
+
+      return cr;
+    }
+
+    /// <summary>
+    /// Calculate intrinsic camera parameters for correspondences in each view.
+    /// </summary>
+    /// <param name="c">Model/image correspondences for multiple views</param>
+    /// <param name="intrinsics">Intrinsic camera parameter</param>
+    /// <returns>Calibration result</returns>
+    public static CalibrationResult CalculateExtrinsics(
+      Correspondences c,
+      Emgu.CV.IntrinsicCameraParameters intrinsics) 
+    {
+      if (c.CorrespondenceCount < 4)
+        throw new ArgumentException("At least four correspondences are required");
+
+      CalibrationResult cr = new CalibrationResult();
+      cr.Correspondences = c;
+      cr.Intrinsics = intrinsics;
+
+      List<Emgu.CV.ExtrinsicCameraParameters> ecp = new List<Emgu.CV.ExtrinsicCameraParameters>();
+      for (int i = 0; i < c.ViewCount; ++i) {
+        ecp.Add(
+          Emgu.CV.CameraCalibration.FindExtrinsicCameraParams2(
+            c.ModelPoints[i], c.ImagePoints[i],
+            intrinsics));
+      }
+      cr.Extrinsics = ecp.ToArray();
+
+      // to calculate the projection error see
+      // http://tech.groups.yahoo.com/group/OpenCV/message/76036
+
+      return cr;
+    }
+
+  }
+}
---
+++ trunk/Calib3D/CalibrationResult.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Calib3D {
+
+  /// <summary>
+  /// Holds the calibration result.
+  /// </summary>
+  public class CalibrationResult {
+    private Emgu.CV.IntrinsicCameraParameters _intrinsics;
+    private Emgu.CV.ExtrinsicCameraParameters[] _extrinsics;
+    private Correspondences _correspondences;
+    private float _error;
+
+    /// <summary>
+    /// Construct empty calibration result.
+    /// </summary>
+    public CalibrationResult() {
+      _intrinsics = null;
+      _extrinsics = null;
+      _error = float.MaxValue;
+    }
+
+    /// <summary>
+    /// Get/Set the reprojection error.
+    /// </summary>
+    /// <remarks>
+    /// The sum of squared distances between the observed 
+    /// projections (image points) and the projected model points.
+    /// </remarks>
+    public float ReprojectionError {
+      get { return _error; }
+      set { _error = value; }
+    }
+
+    /// <summary>
+    /// Get/Set intrinsic camera parameters.
+    /// </summary>
+    public Emgu.CV.IntrinsicCameraParameters Intrinsics {
+      get { return _intrinsics; }
+      set { _intrinsics = value; }
+    }
+
+    /// <summary>
+    /// Get/Set extrinsic camera paramters for each view.
+    /// </summary>
+    public Emgu.CV.ExtrinsicCameraParameters[] Extrinsics {
+      get { return _extrinsics; }
+      set { _extrinsics = value; }
+    }
+
+    /// <summary>
+    /// Get/Set the correspondences associated with this result.
+    /// </summary>
+    public Correspondences Correspondences {
+      get { return _correspondences; }
+      set { _correspondences = value; }
+    }
+  }
+}
