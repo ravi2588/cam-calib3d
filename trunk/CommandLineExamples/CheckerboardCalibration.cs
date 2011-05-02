4fdb760959d25c277618576232b8f104dfee3558
christoph.heindl <christoph.heindl@99fb1f6b-97a0-cadf-a99d-5275acf9f181>
2011-05-02 06:31:01 +0000

    initial commit of examples app

---
+++ trunk/CommandLineExamples/CheckerboardCalibration.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Test.CommandLineParsing;
+using System.ComponentModel;
+
+namespace CommandLineExamples {
+
+  /// <summary>
+  /// Simple Chessboard calibration
+  /// </summary>
+  public class CheckerboardCalibration : IExample {
+
+    /// <summary>
+    /// Parsed arguments
+    /// </summary>
+    class CommandLineArguments {
+
+      [Description("Print this help")]
+      public bool? Help { get; set; }
+
+      [Description("Directory containing the calibration images")]
+      public string ImageDirectory { get; set; }
+
+      [Description("Size of a single square in units of your choice")]
+      public float? PatternSquareLength { get; set; }
+
+      [Description("Number of inner corners per row and column")]
+      public System.Drawing.Size? PatternSize { get; set; }
+    }
+
+    /// <summary>
+    /// Example description
+    /// </summary>
+    public string Description {
+      get { return "Performs camera calibration based on checkerboard patterns."; }
+    }
+
+    public void Run(string[] args) {
+      CommandLineArguments a = new CommandLineArguments();
+      CommandLineParser.ParseArguments(a, args);
+
+      if (a.Help.GetValueOrDefault(false)) {
+        CommandLineParser.PrintUsage(a);
+        return;
+      }
+
+      string image_dir = a.ImageDirectory == null ? "./" : a.ImageDirectory;
+      float size = a.PatternSquareLength.GetValueOrDefault(15);
+      System.Drawing.Size corners = a.PatternSize.GetValueOrDefault(new System.Drawing.Size(9, 6));
+      
+      // Prepare pattern and detector
+      Calib3D.CheckerBoard.CheckerBoardPattern pattern = new Calib3D.CheckerBoard.CheckerBoardPattern();
+      pattern.CornerCount = corners;
+      pattern.SquareLength = size;
+
+      Calib3D.CheckerBoard.CheckerBoardDetector detect = new Calib3D.CheckerBoard.CheckerBoardDetector();
+      detect.Pattern = pattern;
+
+      // Load all images and collect model/image correspondences.
+      System.Drawing.Size image_size = System.Drawing.Size.Empty;
+      Calib3D.Correspondences c = new Calib3D.Correspondences();
+      foreach (Emgu.CV.Image<Emgu.CV.Structure.Bgr, byte> i in Calib3D.IO.Images.FromPath(image_dir)) {
+        image_size = i.Size;
+        Calib3D.DetectionResult dr = detect.FindPattern(i);
+        c.AddView(dr);
+      }
+
+      // Perform intrinsic calibration 
+      Calib3D.CalibrationResult cr = Calib3D.Calibration.GetIntrinsics(c, image_size);
+      System.Console.WriteLine(String.Format("Reprojection Error {0}", cr.ReprojectionError));
+
+    }
+  }
+}
---
+++ trunk/CommandLineExamples/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+using Microsoft.Test.CommandLineParsing;
+
+
+namespace CommandLineExamples {
+  /// <summary>
+  /// Defines the interface for an example.
+  /// </summary>
+  [InheritedExport]
+  public interface IExample {
+    /// <summary>
+    /// Get example description.
+    /// </summary>
+    string Description { get; }
+
+    /// <summary>
+    /// Run example.
+    /// </summary>
+    /// <param name="args">Commandline arguments</param>
+    void Run(string[] args);
+  }
+
+  class Program {
+    /// <summary>
+    /// Let MEF import all available examples.
+    /// </summary>
+    [ImportMany(typeof(IExample))]
+    public IEnumerable<IExample> Examples { get; set; }
+
+    static void Main(string[] args) {
+      Program p = new Program();
+
+      // Compose
+      var catalog = new AssemblyCatalog(System.Reflection.Assembly.GetExecutingAssembly());
+      var container = new CompositionContainer(catalog);
+      var batch = new CompositionBatch();
+      batch.AddPart(p);
+      container.Compose(batch);
+
+      if (String.Compare(args[0], "list", StringComparison.InvariantCultureIgnoreCase) == 0) {
+        Console.WriteLine();
+        Console.WriteLine("Registered Examples");
+        foreach (IExample e in p.Examples) {
+          Console.WriteLine(String.Format("{0} - {1}", e.GetType().Name, e.Description));
+        }
+      }
+      else if (String.Compare(args[0], "run", StringComparison.InvariantCultureIgnoreCase) == 0) {
+        IExample e = p.Examples.FirstOrDefault(ex => ex.GetType().Name == args[1]);
+        if (e != null) {
+          e.Run(args.Skip(2).ToArray());
+        }
+        else {
+          Console.WriteLine(String.Format("{0} unknown", args[1]));
+        }
+      }
+    }
+  }
+}
