dd793ed8f33b6286a005f12f51479db1e65eec26
christoph.heindl <christoph.heindl@99fb1f6b-97a0-cadf-a99d-5275acf9f181>
2011-05-02 13:40:22 +0000

    added support for rendering extrinsic calibration result.

---
+++ trunk/Calib3D/Renderer/CalibrationResultRenderer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Calib3D.Renderer {
+
+  /// <summary>
+  /// Renders intrinsic/extrinsic calibration results.
+  /// </summary>
+  public abstract class CalibrationResultRenderer {
+    private CalibrationResult _cr;
+
+    /// <summary>
+    /// Construct from result.
+    /// </summary>
+    /// <param name="dr">Calibration result to render</param>
+    public CalibrationResultRenderer(CalibrationResult cr) {
+      _cr = cr;
+    }
+
+    /// <summary>
+    /// Get the associated calibration result.
+    /// </summary>
+    public CalibrationResult CalibrationResult {
+      get { return _cr; }
+    }
+
+    /// <summary>
+    /// Render result to image.
+    /// </summary>
+    /// <param name="i">Image to overlay with result</param>
+    /// <param name="view_id">View-id to use</param>
+    public abstract void Render(Emgu.CV.Image<Emgu.CV.Structure.Bgr, byte> i, int view_id);
+  }
+}
