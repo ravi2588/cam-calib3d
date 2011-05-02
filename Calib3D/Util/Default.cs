26089149b41dd99985838a0f4315ed67e8706cbc
christoph.heindl <christoph.heindl@99fb1f6b-97a0-cadf-a99d-5275acf9f181>
2011-05-02 18:20:29 +0000

    added unified default value access.

---
+++ Calib3D/Util/Default.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Calib3D.Util {
+
+  /// <summary>
+  /// Provides a unique interface to default values for refernce and value types.
+  /// </summary>
+  /// <remarks>
+  /// To my knowledge, .NET does not provide any unified access to 
+  /// defaults for reference types. Hence this class. It unifies
+  /// the interface to access custom default values for value types and reference
+  /// types.
+  /// 
+  /// For value types, the method GetValueOrDefault of System.Nullable can be used.
+  /// For reference types, this class emulates GetValueOrDefault by comparing the
+  /// the reference type against null. In case it is null, the user provided default
+  /// value is returned, otherwise the object itself.
+  /// </remarks>
+  public static class Default {
+
+    public static T Get<T>(Nullable<T> v) where T : struct {
+      return v.GetValueOrDefault();
+    }
+
+    public static T Get<T>(Nullable<T> v, T default_value) where T : struct {
+      return v.GetValueOrDefault(default_value);
+    }
+
+    public static T Get<T>(T v, T default_value) where T : class {
+      if (v == null) {
+        return default_value;
+      } else {
+        return v;
+      }
+    }
+
+  }
+}
