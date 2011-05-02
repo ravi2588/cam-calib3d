55aea8ba3fbf107ce3a5cf6ac10a768cca67d802
christoph.heindl <christoph.heindl@99fb1f6b-97a0-cadf-a99d-5275acf9f181>
2011-05-02 18:20:08 +0000

    added pretty printing of calibration results.

---
+++ Calib3D/IO/PrettyPrintExtensions.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Calib3D.IO.Extensions {
+
+  /// <summary>
+  /// Provides formatting extensions for various datatypes and formats.
+  /// </summary>
+  public static class PrettyPrintExtensions {
+
+    /// <summary>
+    /// Pretty print a matrix.
+    /// </summary>
+    /// <param name="matrix">Matrix</param>
+    /// <returns>Formatting string</returns>
+    public static string PrettyPrint(this Emgu.CV.Matrix<double> matrix) 
+    {
+      StringBuilder sb = new StringBuilder();
+
+      for (int i = 0; i < matrix.Rows; ++i) {
+        for (int j = 0; j < matrix.Cols; ++j) {
+          sb.AppendFormat("{0,10:f4} ", matrix[i, j]);
+        }
+        sb.Remove(sb.Length - 1, 1);
+        if (i < matrix.Rows - 1)
+          sb.AppendLine();
+      }
+      return sb.ToString();
+    }
+
+    /// <summary>
+    /// Pretty print intrinsic matrix values and distortion coefficients.
+    /// </summary>
+    /// <param name="i"></param>
+    /// <returns>Formatting string</returns>
+    public static string PrettyPrint(this Emgu.CV.IntrinsicCameraParameters i) 
+    {
+      StringBuilder sb = new StringBuilder();
+      sb.AppendLine("Intrinsic Matrix");
+      sb.AppendLine(i.IntrinsicMatrix.PrettyPrint());
+      sb.AppendLine("Radial Distortion");
+      sb.AppendFormat("  k1={0:f6}, k2={1:f6}", i.DistortionCoeffs[0, 0], i.DistortionCoeffs[1, 0]);
+      if (i.DistortionCoeffs.Rows == 5) {
+        sb.AppendFormat(", k3={0:f6}", i.DistortionCoeffs[4, 0]);
+        sb.AppendLine();
+      } else if (i.DistortionCoeffs.Rows == 8) {
+        sb.AppendFormat(", k3={0:f6}, k4={0:f6}, k5={1:f6}, k6={1:f6}", 
+          i.DistortionCoeffs[4, 0], i.DistortionCoeffs[5, 0], i.DistortionCoeffs[6, 0], i.DistortionCoeffs[7, 0]);
+        sb.AppendLine();
+      }
+      sb.AppendLine("Tangential Distortion");
+      sb.AppendFormat("  p1={0:f6}, p2={1:f6}", i.DistortionCoeffs[2, 0], i.DistortionCoeffs[3, 0]);
+      
+      return sb.ToString();
+    }
+
+    /// <summary>
+    /// Pretty print intrinsics using human readable values.
+    /// </summary>
+    /// <param name="i"></param>
+    /// <param name="image_width">Image width</param>
+    /// <param name="image_height">Image height</param>
+    /// <param name="chip_width">Chip width</param>
+    /// <param name="chip_height">Chip height</param>
+    /// <returns>Formatting string</returns>
+    public static string PrettyPrint(
+      this Emgu.CV.IntrinsicCameraParameters i, 
+      int image_width, int image_height, 
+      double chip_width, double chip_height) 
+    {    
+      double fovx, fovy, focal_length, pixel_aspect_ratio;
+      Emgu.CV.Structure.MCvPoint2D64f principal_point;
+
+      i.GetIntrinsicMatrixValues(
+        image_width, image_height, chip_width, chip_height,
+        out fovx, out fovy, out focal_length, out principal_point, out pixel_aspect_ratio);
+
+      StringBuilder sb = new StringBuilder();
+      sb.AppendLine("Focal Length");
+      sb.AppendFormat("  f={0:f6}", focal_length);
+      sb.AppendLine();
+      sb.AppendLine("Principal Point");
+      sb.AppendFormat("  x={0:f6} y={0:f6}", principal_point.x, principal_point.y);
+      sb.AppendLine();
+      sb.AppendLine("Field of View");
+      sb.AppendFormat("  x-angle={0:f6}, y-angle{1:f6}", fovx, fovy);
+
+      return sb.ToString();
+    }
+
+    /// <summary>
+    /// Pretty print extrinsic matrix values.
+    /// </summary>
+    /// <param name="i"></param>
+    /// <returns>Formatting string</returns>
+    public static string PrettyPrint(this Emgu.CV.ExtrinsicCameraParameters e) {
+      StringBuilder sb = new StringBuilder();
+      sb.AppendLine("Extrinsic Matrix");
+      sb.AppendLine(e.ExtrinsicMatrix.PrettyPrint());
+
+      return sb.ToString();
+    }
+
+  }
+}
