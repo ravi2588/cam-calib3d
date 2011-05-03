4b693eb1d14c4434f1bd68c9d661e9831407ee9d
christoph.heindl <christoph.heindl@99fb1f6b-97a0-cadf-a99d-5275acf9f181>
2011-05-03 05:46:31 +0000

    added marker detection example.

---
+++ trunk/CommandLineExamples/LiveMarkerDetection.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+using Microsoft.Test.CommandLineParsing;
+using Calib3D.Util;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using System.Threading;
+
+namespace CommandLineExamples {
+
+  /// <summary>
+  /// Marker detection in live stream from camera.
+  /// </summary>
+  /// <example>
+  /// CommandLineExamples.exe run LiveMarkerDetection /MarkerImage=../etc/patterns/marker/marker_a.png
+  /// </example>
+  public class LiveMarkerDetection : IExample {
+
+    /// <summary>
+    /// Supported commandline arguments.
+    /// </summary>
+    class CommandLineArguments {
+
+      [Description("Print this help")]
+      public bool? Help { get; set; }
+
+      [Description("Path to marker image")]
+      public string MarkerImage { get; set; }
+
+      [Description("Size of marker")]
+      public float? MarkerLength { get; set; }
+
+      [Description("Device ID of camera")]
+      public int? HardwareId { get; set; }
+    }
+
+    public string Description {
+      get { return "Marker detection in live stream from camera."; }
+    }
+
+    public void Run(string[] args) {
+      CommandLineArguments a = new CommandLineArguments();
+      CommandLineParser.ParseArguments(a, args);
+
+      if (a.Help.GetValueOrDefault(false)) {
+        CommandLineParser.PrintUsage(a);
+        return;
+      }
+
+      string image_path = Default.Get(a.MarkerImage, "marker.png");
+      int device_id = Default.Get(a.HardwareId, 0);
+      float marker_length = Default.Get(a.MarkerLength, 50);
+
+      // Setup pattern and detector
+      Calib3D.Marker.MarkerPattern pat = new Calib3D.Marker.MarkerPattern();
+      pat.MarkerImage = Calib3D.IO.Images.FromPath(image_path).First();
+      pat.MarkerLength = marker_length;
+
+      Calib3D.Marker.MarkerDetector det = new Calib3D.Marker.MarkerDetector();
+      det.BinaryThreshold = 40;
+      det.MaximumErrorNormed = 0.4f;
+      det.Pattern = pat;
+
+      Calib3D.IO.Images.Show(pat.MarkerImage, 1, "Marker");
+
+      // Start capturing from camera.
+      Capture capture = new Capture(device_id);
+
+      while (!Console.KeyAvailable) {
+        Emgu.CV.Image<Bgr, byte> i = capture.QueryFrame();
+        Calib3D.DetectionResult dr = det.FindPattern(i);
+        dr.ResultRenderer.Render(i);
+        Calib3D.IO.Images.Show(i, 30, "LiveFeed");
+      }
+
+    }
+  }
+}
